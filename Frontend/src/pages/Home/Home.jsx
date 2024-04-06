d68ac82e7e7dcd141b93a821a9db90ba46e85bb5
Shivam Kumar Pathak <114474974+Shivampathak-1@users.noreply.github.com>
2024-04-06 19:39:25 +0530

    tt

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -4,9 +4,9 @@ function Home() {
   return (
     <>
       {/* <h1 className=' bg-slate-500'>This is Home page</h1> */}
-      <div className="container min-h-screen w-full flex" id="home">
-        <div className="container min-h-screen w-full" id="home1">
-          <div className="container min-h-screen w-full" id="home2">
+      <div className="container min-h-screen flex" id="home">
+        <div className="container min-h-screen" id="home1">
+          <div className="container min-h-screen" id="home2">
             <div className="page1">
               <div className="left">
                   <img src="" alt="" />
