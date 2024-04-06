d2511cd58c83c6b89718e969b7f7821492c7186f
Shivam Kumar Pathak <114474974+Shivampathak-1@users.noreply.github.com>
2024-04-06 15:16:31 +0530

    te

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from  "react-router-dom";
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './component/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
 import Services from './pages/Services/Services';
-import Footer from './components/Footer/Footer';
+import Footer from './component/Footer/Footer';
 function App() {
   return (
     <>
