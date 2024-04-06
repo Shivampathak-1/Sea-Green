822beed57141778cdf1eac90dfb8ef7e50aeb675
Manasvi Gaur <manasvigaur3@gmail.com>
2024-04-06 23:56:19 +0530

    about page setup

--- Frontend/src/pages/About/About.jsx
+++ Frontend/src/pages/About/About.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
-function About() {
+import React from "react";
+import "./About.css";
+import img from "../../assets/Mask_group.png";
+function Home() {
   return (
     <>
-     <h1>About Us</h1> 
+      <div className="main">
+        <div className="min-h-screen flex" id="home">
+          <div className="w-full p-0 min-h-screen" id="home1">
+            <div className="w-full p-0 min-h-screen" id="home2">
+            <div className="right">
+                  <img src={img} alt="" id="patch1" />
+                  <div >
+                  <p className="text-white text-4xl md:text-6xl font-bold pt-10 md:pt-16 lg:pt-7 xl:pt-9 z-50 md:pr-0 xl:pr-9">About Us</p>
+
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default About
+export default Home;
