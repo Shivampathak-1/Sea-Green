d54731b3ce39f41f293588dcbd99322f26ca7107
Shivam Kumar Pathak <114474974+Shivampathak-1@users.noreply.github.com>
2024-04-06 23:26:55 +0530

    ch2

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -4,19 +4,23 @@ import img from "../../assets/img1.png";
 function Home() {
   return (
     <>
-      {/* <h1 className=' bg-slate-500'>This is Home page</h1> */}
-      <div className="main">
-        <div className="min-h-screen flex" id="home">
-          <div className="w-full p-0 min-h-screen" id="home1">
-            <div className="w-full p-0 min-h-screen" id="home2">
-              <div className="page1">
-                <div className="left h-96">
-                    <h1>Welcome to our website!</h1>
-                </div>
-                <div className="right"></div>
-                <div className="button_start">
+      <div className="min-h-screen flex" id="home">
+        <div className="w-full p-0 min-h-screen" id="home1">
+          <div className="w-full p-0 min-h-screen" id="home2">
+            <div className="page1">
+              <div className="left">
+                {/* Here */}
+                <img src={img} alt="patch" className="patch" />
+                <span className="text1">Discovering the</span>
+                <span className="text2">Depths</span>
+                <span className="text3">Unveiling the Secrets of Plastic Pollution in Our Oceans</span>
+                {/* <div className="button_start">
                   <button className="button center">Let's Start</button>
-                </div>
+                </div> */}
+              </div>
+              <div className="right"></div>
+              <div className="button_start">
+                <button className="button center">Let's Start</button>
               </div>
             </div>
           </div>
