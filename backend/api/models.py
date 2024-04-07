a7d671a5928232af7a1c6180fd86e5a6a90f2578
Nikhil Kumar <thenikhilkumar1@gmail.com>
2024-04-07 09:04:42 +0530

    COMMIT

--- backend/api/models.py
+++ backend/api/models.py
@@ -23,6 +23,7 @@ class User(AbstractUser):
 class Images(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='predict_images/')
+    pred_image = models.ImageField(upload_to='predicted_images/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,14 @@ from .services import userLoginService
 from django.utils.decorators import method_decorator
 from django.views.decorators.http import require_POST
 import os
+import shutil
+from django.core.files import File
 from ultralytics import YOLO
+import cv2
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
@@ -62,13 +69,56 @@ class Detect(APIView):
             user = User.objects.get(id = request.data.get('user_id'))
             if user == None:
                 return Response({"status":"unauthorized","error":"login required"}, status=status.HTTP_401_UNAUTHORIZED)
-            image = Images.objects.filter(user=request.data.get('user_id')).order_by("-created_at")[0]
+            image = Images.objects.filter(user=request.data.get('user_id'))
+            if image==None:
+                return Response({"status":"NAN","message":"Image not provided"})
+            image = image.order_by("-created_at")[0]
             ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
             MODEL = os.path.join(ROOT_DIR, 'YOLO_Custom_v8m.pt')
             model = YOLO(MODEL)
             img_url = image.image
             TEST = os.path.join(ROOT_DIR, img_url.path)
             model.predict(TEST, save=True, conf = 0.37, show = True)
-            return Response({"status":"done"}, status=status.HTTP_200_OK)
+            current_dir = os.getcwd()
+            parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+            img_str = img_url.path.split("\\")[-1]
+            pred_image = os.path.join(parent_dir, f'runs/detect/predict/{img_str}')
+            if os.path.exists(pred_image):
+                image.pred_image.save(os.path.basename(pred_image), open(pred_image, 'rb'), save=True)
+                shutil.rmtree(os.path.join(parent_dir, f'runs/detect/predict'))
+                return Response({"status":"ok", "message":"predicted", "url":(image.pred_image.path.split('\\')[-2]+"/"+image.pred_image.path.split('\\')[-1])})
+            else:
+                return Response({"status":"not ok", "message":"image not found"})
         except Exception as e:
-            return Response({"status":"not ok", "error":e})
+            print("ex: ",e)
+            return Response({"status":"not ok", "error":"something went wrong"})
+   
+   
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL = os.path.join(ROOT_DIR, 'YOLO_Custom_v8m.pt')
+model = YOLO(MODEL)  
+      
+@csrf_exempt
+def predict_from_webcam(request):
+    # Open webcam
+    # Initialize webcam
+
+    while True: # Read frame from webcam
+
+        # Perform object detection on the frame using YOLOv8
+        objects = model.predict(source=0, stream=True, save=True, conf = 0.37, show = True)
+
+        # Convert detections to JSON format
+        # detections_json = {
+        #     "objects": objects
+        # }
+        objects_list = list(objects)
+
+# Serialize the list to JSON
+        # json_data = json.dumps({'objects': objects_list})
+
+        # # Return JSON response with detections
+        # return Response(json_data)
+
+    # Release webcam
+    cap.release()
