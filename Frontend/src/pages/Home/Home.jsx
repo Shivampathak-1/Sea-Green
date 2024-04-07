4edd7c3ef321b453706366e02dd982bfae5fb3b5
Shivam Kumar Pathak <114474974+Shivampathak-1@users.noreply.github.com>
2024-04-07 07:00:09 +0530

    tt

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -19,15 +19,13 @@ function Home() {
             <div className="discover">
               <a href="/">Discover</a>
             </div>
-            <div className="page2">
-              <iframe title="hackskill" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=0fb091d3-fc98-4cd5-a98c-6b9784037978&autoAuth=true&ctid=2c5bdaf4-8ff2-4bd9-bd54-7c50ab219590" frameborder="0" allowFullScreen="true"></iframe>
-            <div className="page2 mt-36">
             <img src={img2} alt="patch2" className="patch2" />
             <div className="button_start">
               <button className="button center">Let's Start</button>
             </div>
           </div>
         </div>
+
         <div className="page2 h-screen">
           <p>Real Time between Year vs Marine plastic pollution</p>
           <iframe
