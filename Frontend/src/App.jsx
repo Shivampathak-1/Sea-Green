7fe16234fc99b7f6468d5b595b14fe176db5427c
Manasvi Gaur <manasvigaur3@gmail.com>
2024-04-07 08:40:42 +0530

    d

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,12 +12,12 @@ function App() {
     <>
       <Router>
         <Navbar/>
-        {/* <Routes>
+        <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/services" element={<Services />}></Route>
-        </Routes> */}
+        </Routes>
         <InsertionPage/>
         <Footer />
       </Router>
