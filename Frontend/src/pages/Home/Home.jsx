6d269c9eab21cb28c52227b2cb1cbea5fba95aad
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-04-07 10:55:59 +0530

    button

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
             </div>
             <img src={img2} alt="patch2" className="patch2" />
             <div className="button_start">
-              <a href="/let-start" className="button center">Let's Start</a>
+              <a href="/let-start" className="buttonmain center">Let's Start</a>
             </div>
           </div>
         </div>
